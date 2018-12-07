4e3cb0610412cb0c157673a504ef8de1b7413e0c
ohgo1 <45685983+ohgo1@users.noreply.github.com>
2018-12-07 04:11:30 -0600

    Update hw8.java

--- hw8.java
+++ hw8.java
@@ -43,19 +43,20 @@ public class hw8
 public static void main(String[] args) {
 
 
- int[] board = new int[15]; 
- ArrayList<Integer> fillP = new ArrayList<Integer>();
- ArrayList<Integer> freeP = new ArrayList<Integer>();
- ArrayList<int[]> moves = new ArrayList<int[]>();
+ int[] board = new int[15]; 					//Declare a new board
+ ArrayList<Integer> fillP = new ArrayList<Integer>();		//An arraylist for the filled pegs
+ ArrayList<Integer> freeP = new ArrayList<Integer>();		//An arraylist for the free spaces
+ ArrayList<int[]> moves = new ArrayList<int[]>();		//An arraylist for the possible moves
  int numPegs = 0;
  
-for (int j=0; j<15; j++)
+	
+	for (int j=0; j<15; j++)
 		{
 			fillP.add(j);				// set all of the filled spots to the index
 			board[j]= 0;				// set these indexes to default at 0
 		}
 
- for(int i=0;i<15;i++)
+ 	for(int i=0;i<15;i++)
 		{  
 			board[i] = 1;   			// set all spots to 1
 			fillP.add(i);  				// add all the 1 indexes to filled			
@@ -80,31 +81,29 @@ for (int j=0; j<15; j++)
 			
 				for(int j=0;j<14;j++)
 				{
-					//board[endlist.get(j)[0]]= 1;
-					//board[endlist.get(j)[1]]= 1;
-					//board[endlist.get(j)[2]]= 0;
+					//go through the moves and show the steps
 					
 				}
-				   //Print out the steps to the solution
-					System.out.println("    "+board[0]+"    ");
-					System.out.println("   "+board[1]+" "+board[2]+"   ");
-					System.out.println("  "+board[3]+" "+board[4]+" "+board[5]+"  ");
-					System.out.println(" "+board[6]+" "+board[7]+" "+board[8]+" "+board[9]+" ");
-					System.out.println(board[10]+" "+board[11]+" "+board[12]+" "+board[13]+" "+board[14]+"\n");
+				 //Print out the steps to the solution
+				System.out.println("    "+board[0]+"    ");
+				System.out.println("   "+board[1]+" "+board[2]+"   ");
+				System.out.println("  "+board[3]+" "+board[4]+" "+board[5]+"  ");
+				System.out.println(" "+board[6]+" "+board[7]+" "+board[8]+" "+board[9]+" ");
+				System.out.println(board[10]+" "+board[11]+" "+board[12]+" "+board[13]+" "+board[14]+"\n");
 			
 	}
-					// Print out the solution
-					System.out.println("\nSolution\n\n");
-					System.out.println("    "+board[0]+"    ");
-					System.out.println("   "+board[1]+" "+board[2]+"   ");
-					System.out.println("  "+board[3]+" "+board[4]+" "+board[5]+"  ");
-					System.out.println(" "+board[6]+" "+board[7]+" "+board[8]+" "+board[9]+" ");
-					System.out.println(board[10]+" "+board[11]+" "+board[12]+" "+board[13]+" "+board[14]+"\n");
+			// Print out the solution
+			System.out.println("\nSolution\n\n");
+			System.out.println("    "+board[0]+"    ");
+			System.out.println("   "+board[1]+" "+board[2]+"   ");
+			System.out.println("  "+board[3]+" "+board[4]+" "+board[5]+"  ");
+			System.out.println(" "+board[6]+" "+board[7]+" "+board[8]+" "+board[9]+" ");
+			System.out.println(board[10]+" "+board[11]+" "+board[12]+" "+board[13]+" "+board[14]+"\n");
 }
 
 public static int move(int[] gameboard, ArrayList<int[]> moveslist, ArrayList<Integer> filledPegs, ArrayList<Integer> freePegs, int ctr)
 {
-	int[] tempBoard  = new int[15];											// Temp variables for the board, free, filled spaces, and movelist
+	int[] tempBoard  = new int[15];					// Temp variables for the board, free, filled spaces, and movelist
 	ArrayList<Integer> tempFilled  = new ArrayList<Integer>(filledPegs);	
 	ArrayList<Integer> tempFree  = new ArrayList<Integer>(freePegs);		
 	ArrayList<int[]> tempMlist  = new ArrayList<int[]>(moveslist);			
@@ -127,13 +126,13 @@ public static int move(int[] gameboard, ArrayList<int[]> moveslist, ArrayList<In
             {
 
                 tempCtr++;
-                tempMlist.add(MOVES[j]);	//from, over, to
+                tempMlist.add(MOVES[j]);						//from, over, to
 				tempFilled.remove(Integer.valueOf(MOVES[j][0]));  	// remove the jumping piece
 				tempFilled.remove(Integer.valueOf(MOVES[j][1]));	// remove the jumped piece
-				tempFree.add(MOVES[j][0]);							// add the jumping piece to free list
-				tempFree.add(MOVES[j][1]);							// add the jumped piece to the free spot list
+				tempFree.add(MOVES[j][0]);				// add the jumping piece to free list
+				tempFree.add(MOVES[j][1]);				// add the jumped piece to the free spot list
 				tempFree.remove(Integer.valueOf(MOVES[j][2]));		// remove the previously free spot from the free list
-				tempFilled.add(MOVES[j][2]);						// add that same spot to the filled list
+				tempFilled.add(MOVES[j][2]);				// add that same spot to the filled list
 				
 
                 if(move(tempBoard,  tempMlist, tempFilled, tempFree, tempCtr)==1)
